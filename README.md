747c60293739635985680b68e15232283cde39ec
DixonOrtiz <dixon.ortizc@gmail.com>
2020-12-30 23:47:28 -0300

    script finished

---
+++ README.md
@@ -0,0 +1,16 @@
+# Palindromes
+
+## Steps to run
+
+### Clone Repository
+
+`git clone https://github.com/DixonOrtiz/companies-api`
+
+### Run with node
+
+`node palindrome.js`
+
+### Note
+
+- I considered strings that were at least 3 characters long
+- No meaningful words or phrases were found, but when testing with other texts, yes, so it is assumed that there was no such thing in the given text
---
+++ palindrome.js
@@ -0,0 +1,29 @@
+const isPalindrome = (text) => {
+  for (let i = 0; i < text.length; i++) {
+    if (text[i] !== text[text.length - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const evaluateText = (text) => {
+  let counter = 0;
+  for (let i = 0; i < textToEvaluate.length; i++) {
+    for (let j = i + 1; j < textToEvaluate.length; j++) {
+      let subText = textToEvaluate.substring(i, j);
+      if (isPalindrome(subText) && subText.length > 2) {
+        counter += 1;
+        console.log(`${subText} is a palindrome!`);
+      }
+    }
+  }
+  console.log(
+    `\n${counter} strings have been found that are read the same from left to right!`
+  );
+};
+
+const textToEvaluate =
+  'afoolishconsistencyisthehobgoblinoflittlemindsadoredbylittlestatesmenandphilosophersanddivineswithconsistencyagreatsoulhassimplynothingtodohemayaswellconcernhimselfwithhisshadowonthewallspeakwhatyouthinknowinhardwordsandtomorrowspeakwhattomorrowthinksinhardwordsagainthoughitcontradicteverythingyousaidtodayahsoyoushallbesuretobemisunderstoodisitsobadthentobemisunderstoodpythagoraswasmisunderstoodandsocratesandjesusandlutherandcopernicusandgalileoandnewtonandeverypureandwisespiritthatevertookfleshtobegreatistobemisunderstood';
+
+evaluateText(textToEvaluate);
